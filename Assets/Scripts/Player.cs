4434ddd37192216e92c259ea53b351a34695f9c2
Erison Veshi <erisonve@gmail.com>
2023-10-24 03:07:38 +0200

    Add collision dedection for player character

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,7 +12,41 @@ public class Player : MonoBehaviour {
     private void Update() {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDirection *moveSpeed *Time.deltaTime;
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirection, moveDistance);
+
+        if (!canMove) {
+            // Cant move in forward direction
+
+            // Attempt to move sideways
+            Vector3 moveDirectionX = new Vector3(moveDirection.x, 0, 0);
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionX, moveDistance);
+
+            if (canMove) {
+                // Can move only on the X direction
+                moveDirection = moveDirectionX;
+            } else {
+                // Cannot move only on the X direction
+
+                // Atempt only Z direction
+                Vector3 moveDirectionZ = new Vector3(0, 0, moveDirection.z);
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionZ, moveDistance);
+
+                if (canMove) {
+                    // Can move only on Z direction
+                    moveDirection = moveDirectionZ;
+                } else { 
+                    // Cant move in any direnction
+                }
+            }
+        }
+
+        if (canMove) {
+            transform.position += moveDirection * moveDistance;
+        }
 
         isWalking = moveDirection != Vector3.zero;
 
