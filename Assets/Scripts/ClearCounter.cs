2cc9d04212763e52b5e3ff9ea3a3026b9a5e3bd1
Erison Veshi <erisonve@gmail.com>
2023-10-30 03:09:39 +0100

    Move KitchenObject for parent to new parent

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -7,12 +7,30 @@ public class ClearCounter : MonoBehaviour {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform counterTopPoint;
 
+    private KitchenObject kitchenObject;
+
     public void Interact() {
-        Debug.Log("Interact");
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-        kitchenObjectTransform.localPosition = Vector3.zero;
+        if (kitchenObject == null) {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+        } else {
+            Debug.Log(kitchenObject.GetClearCounter());
+        }
+    }
 
-        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSo().objectName);
+    public Transform GetKitchenObjectFollowTransform() {
+        return counterTopPoint;
+    }
 
+    public void SetKitchenObject(KitchenObject kitchenObject) {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject() { 
+        return kitchenObject; 
+    }
+
+    public void ClearKitchenObject() { 
+        this.kitchenObject = null;
     }
 }
